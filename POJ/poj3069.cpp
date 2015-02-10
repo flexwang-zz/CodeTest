59a9f820e81648dd46e25aca8cbf1743f3c78d17
flexwang <zhaotianzju@gmail.com>
2015-02-10 08:35:35 +0800

    'poj'

--- POJ/poj3069.cpp
+++ POJ/poj3069.cpp
@@ -1,6 +1,33 @@
-#define POJ_3069
-#ifdef POJ_3069
+/*
+Description
 
+Saruman the White must lead his army along a straight path from Isengard to Helm’s Deep. To keep track of his forces, Saruman distributes seeing stones, known as palantirs, among the troops. Each palantir has a maximum effective range of R units, and must be carried by some troop in the army (i.e., palantirs are not allowed to “free float” in mid-air). Help Saruman take control of Middle Earth by determining the minimum number of palantirs needed for Saruman to ensure that each of his minions is within R units of some palantir.
+
+Input
+
+The input test file will contain multiple cases. Each test case begins with a single line containing an integer R, the maximum effective range of all palantirs (where 0 ≤ R ≤ 1000), and an integer n, the number of troops in Saruman’s army (where 1 ≤ n ≤ 1000). The next line contains n integers, indicating the positions x1, …, xn of each troop (where 0 ≤ xi ≤ 1000). The end-of-file is marked by a test case with R = n = −1.
+
+Output
+
+For each test case, print a single integer indicating the minimum number of palantirs needed.
+
+Sample Input
+
+0 3
+10 20 20
+10 7
+70 30 1 7 15 20 50
+-1 -1
+Sample Output
+
+2
+4
+Hint
+
+In the first test case, Saruman may place a palantir at positions 10 and 20. Here, note that a single palantir with range 0 can cover both of the troops at position 20.
+
+In the second test case, Saruman can place palantirs at position 7 (covering troops at 1, 7, and 15), position 20 (covering positions 20 and 30), position 50, and position 70. Here, note that palantirs must be distributed among troops and are not allowed to “free float.” Thus, Saruman cannot place a palantir at position 60 to cover the troops at positions 50 and 70.
+*/
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
@@ -42,5 +69,3 @@ int main()
         printf("%d\n", ans);
     }
 }
-
-#endif
--- POJ/poj3253.cpp
+++ POJ/poj3253.cpp
@@ -1,5 +1,35 @@
-#define POJ_3253
-#ifdef POJ_3253
+/*
+Description
+
+Farmer John wants to repair a small length of the fence around the pasture. He measures the fence and finds that he needs N (1 ≤ N ≤ 20,000) planks of wood, each having some integer length Li (1 ≤ Li ≤ 50,000) units. He then purchases a single long board just long enough to saw into the N planks (i.e., whose length is the sum of the lengths Li). FJ is ignoring the "kerf", the extra length lost to sawdust when a sawcut is made; you should ignore it, too.
+
+FJ sadly realizes that he doesn't own a saw with which to cut the wood, so he mosies over to Farmer Don's Farm with this long board and politely asks if he may borrow a saw.
+
+Farmer Don, a closet capitalist, doesn't lend FJ a saw but instead offers to charge Farmer John for each of the N-1 cuts in the plank. The charge to cut a piece of wood is exactly equal to its length. Cutting a plank of length 21 costs 21 cents.
+
+Farmer Don then lets Farmer John decide the order and locations to cut the plank. Help Farmer John determine the minimum amount of money he can spend to create the N planks. FJ knows that he can cut the board in various different orders which will result in different charges since the resulting intermediate planks are of different lengths.
+
+Input
+
+Line 1: One integer N, the number of planks 
+Lines 2..N+1: Each line contains a single integer describing the length of a needed plank
+Output
+
+Line 1: One integer: the minimum amount of money he must spend to make N-1 cuts
+Sample Input
+
+3
+8
+5
+8
+Sample Output
+
+34
+Hint
+
+He wants to cut a board of length 21 into pieces of lengths 8, 5, and 8. 
+The original board measures 8+5+8=21. The first cut will cost 21, and should be used to cut the board into pieces measuring 13 and 8. The second cut will cost 13, and should be used to cut the 13 into 8 and 5. This would cost 21+13=34. If the 21 was cut into 16 and 5 instead, the second cut would cost 16 for a total of 37 (which is more than 34).
+*/
 #include <stdio.h>
 #include <vector>
 using namespace std;
@@ -52,5 +82,3 @@ int main()
     return 0;
 
 }
-
-#endif
