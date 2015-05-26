e267a8a1b89ecf54e62faf8bdf76ec1ecd84115f
flexwang <zhaotianzju@gmail.com>
2015-05-26 13:19:20 +0800

    leetcode

--- leetcode/Reverse_Integer.cpp
+++ leetcode/Reverse_Integer.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int reverse(int x) {
-        int result = 0; 
-        
-        do
-        {
-            result = result*10 + x%10;
+        int sign = x >= 0 ? 1 : -1;
+        if (x < 0)  x = -x;
+
+        vector<int> digits;
+
+        do{
+            digits.push_back(x % 10);
             x /= 10;
-        }while(x);
-        
-        return result;
+        } while (x>0);
+
+        int ans = 0;
+
+        for (int i = 0; i < digits.size(); i++) {
+            if (ans != ans*10/10) return 0;
+            ans = ans * 10 + digits[i];
+            if (ans < 0) return 0;
+        }
+
+        return ans*sign;
     }
 };
