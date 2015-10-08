6cc5dfe2aa9f73a14c65318cc98c9c27933f5c15
Zhaotian Wang <zhaotianzju@gmail.com>
2015-10-08 22:06:03 +0800

    Update pat1021.cpp

--- PAT/PAT_A/pat1021.cpp
+++ PAT/PAT_A/pat1021.cpp
@@ -10,154 +10,100 @@
  1 3
  1 4
  2 5
-
  Sample Output 1:
  3
  4
  5
-
  Sample Input 2:
  5
  1 3
  1 4
  2 5
  3 4
-
  Sample Output 2:
  Error: 2 components
-
  */
 
-#include<stdio.h>
-#include<vector>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
 using namespace std;
 
-struct Node {
-	vector<int> adjacent;
-};
-
-struct Path {
-	int from, to;
-	Path(int f, int t) :
-			from(f), to(t) {
-	}
-};
-
-struct Node nodes[10001];
-bool search(int rootid, bool known[], int curdepth, int &maxdepth, int &nknown,
-		int fatherid);
-int findroot(int id, int root[]);
-
-int main() {
-	int n;
-	scanf("%d", &n);
-
-	bool known[100001];
-	vector<Path> paths;
-
-	for (int i = 1; i <= n - 1; i++) {
-		int a1, a2;
-		scanf("%d %d", &a1, &a2);
-		nodes[a1].adjacent.push_back(a2);
-		nodes[a2].adjacent.push_back(a1);
-		paths.push_back(Path(a1, a2));
-	}
-
-	int maxdepth = -1;
-	vector<int> deepestroot;
-	int nknown;
-
-	for (int i = 1; i <= n; i++) {
-		int depth = -1;
-		nknown = 0;
-		for (int j = 1; j <= n; j++) {
-			known[j] = false;
-		}
-
-		if (search(i, known, 1, depth, nknown, -1)) {
-			if (maxdepth < depth) {
-				maxdepth = depth;
-				deepestroot.clear();
-				deepestroot.push_back(i);
-//        deepestropot
-			} else if (maxdepth == depth) {
-				deepestroot.push_back(i);
-			}
-//      printf("%d %d\n", i, depth);
-		} else {
-			break;
-//      printf("sorry\n");
-		}
-		if (nknown < n) {
-			break;
-		}
-//    printf("known:%d\n", nknown);
-	}
-
-	if (nknown == n) {
-		for (int i = 0; i < deepestroot.size(); i++) {
-			printf("%d\n", deepestroot[i]);
-		}
-	} else {
-		int ncomponents = n;
-		int root[100001];
-		for (int i = 1; i <= n; i++) {
-			root[i] = i;
-		}
-
-		for (int i = 0; i < paths.size(); i++) {
-			int a = paths[i].from;
-			int b = paths[i].to;
-
-			int root1 = findroot(a, root);
-			int root2 = findroot(b, root);
-
-			if (root1 != root2) {
-				root[root1] = root2;
-				ncomponents--;
-			}
-		}
-		printf("Error: %d components", ncomponents);
-	}
-//  while(1);
-	return 0;
+inline void add_edge(int a, int b, vector<vector<int>>&G) {
+    a--, b--;
+    G[a].push_back(b);
+    G[b].push_back(a);
 }
 
-bool search(int rootid, bool known[], int curdepth, int &maxdepth, int& nknown,
-		int fatherid) {
-	if (known[rootid]) {
-		return false;
-	}
-
-	known[rootid] = true;
-	curdepth++;
-	nknown++;
-	if (curdepth > maxdepth) {
-		maxdepth = curdepth;
-	}
-
-	for (int i = 0; i < nodes[rootid].adjacent.size(); i++) {
-		int id = nodes[rootid].adjacent[i];
-		if (id == fatherid) {
-			continue;
-		}
-//    printf("%d ", id);
-		if (!(search(id, known, curdepth, maxdepth, nknown, rootid))) {
-			return false;
-		}
-	}
-//  printf("\n");
-	return true;
+bool dfs(int id, int p, vector<vector<int>> &G, vector<bool>& visited) {
+    if (visited[id]) return false;
+    visited[id] = true;
+    bool flag = true;
+    for (int i=0; i<G[id].size(); i++) {
+        if (G[id][i] == p) continue;
+        flag &= dfs(G[id][i], id, G, visited);
+    }
+    return flag;
 }
 
-int findroot(int id, int root[]) {
-	int r;
-	while ((r = root[id]) != id) {
-		root[id] = root[r];
-		id = r;
-	}
-
-	return r;
+int get_height(int root, vector<vector<int>> &G) {
+    vector<pair<int,int>> level = {{root,-1}};
+    int h = 1;
+    while (level.size() != 0) {
+        vector<pair<int,int>> temp;
+        for (int i=0; i<level.size(); i++) {
+            int cur = level[i].first;
+            int p = level[i].second;
+            for (int j=0; j<G[cur].size(); j++) {
+                int v = G[cur][j];
+                if (v == p) continue;
+                temp.push_back(make_pair(v, cur));
+            }
+        }
+        level = temp;
+        h++;
+    }
+    return h;
 }
 
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> G(n);
+    for (int i=1; i<n; i++) {
+        int a, b;
+        cin >> a >> b;
+        add_edge(a, b, G);
+    }
+    vector<bool> visited(n, false);
+    int k = 0;
+    bool is_tree = true;
+    for (int i=0; i<n; i++) {
+        if (!visited[i]) {
+            ++k;
+            is_tree &= dfs(i, -1, G, visited);
+        }
+    }
+    if (!is_tree || k > 1) {
+        cout << "Error: "<< k << " components" << endl;
+        return 0;
+    }
+    int height = 0;
+    vector<int> ans;
+    for (int i=0; i<n; i++) {
+        int h;
+        if ((h=get_height(i, G)) > height) {
+            ans = {i};
+            height = h;
+        }
+        else if (h == height)
+            ans.push_back(i);
+    }
+    
+    for (int i=0; i<ans.size(); i++) 
+        cout << ++ans[i] << endl;
+    return 0;
+    
+}
