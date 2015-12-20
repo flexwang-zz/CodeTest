b1e3bf2dae8df53aae136edb290f9d286f06ac80
flexwang <zhaotianzju@gmail.com>
2015-12-20 19:09:59 +0800

    leetcode

--- leetcode/gray-code.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    vector<int> grayCode(int n) {
-        vector<int> ans(1<<n);
-        for (int i=0; i<(1<<n); i++) 
-            ans[i] = i^(i>>1);
-        return ans;
-    }
-};
