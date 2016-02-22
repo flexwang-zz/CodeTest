19f953d5953c460039dc0a17bfdf14670311a53e
Zhaotian Wang <zhaotianzju@gmail.com>
2016-02-22 22:56:07 +0800

    Update basic-calculator-ii.cpp

--- leetcode/basic-calculator-ii.cpp
+++ leetcode/basic-calculator-ii.cpp
@@ -1,63 +1,28 @@
 class Solution {
 public:
+    int nextNum(string &s, int &i) {
+        int num = 0;
+        for (;i<s.length(); i++)
+            if (s[i] >='0' && s[i] <= '9')
+                num = num*10 + s[i]-'0';
+            else if (s[i] == ' ') continue;
+            else break;
+        return num;
+    }
     int calculate(string s) {
-        stack<int> sd;
-        stack<char> sr;
-        s = '('+s;
-        s += ')';
-        int last_d = 0;
-        bool last_n = false;
-        for (int i=0; i<s.length(); i++) {
-            char ch = s[i];
-            if (ch == ' ') continue;
-            if (!isoperator(ch)) {
-                last_d = last_d*10 + ch-'0';
-                last_n = true;
-            }
-            else {
-                if (last_n)
-                    sd.push(last_d);
-                last_d = 0;
-                last_n = false;
-                while (!sr.empty() && diff(sr.top(), ch) < 0) {
-                    char top = sr.top(); 
-                    sr.pop();
-                    if (top == '(' && ch == ')') break;
-                    int b = sd.top();
-                    sd.pop();
-                    int a = sd.top();
-                    sd.pop();
-                    sd.push(calc(a, b, top));
-                }
-                if (ch != ')') sr.push(ch);
-            }
+        int i = 0;
+        int result = 0;
+        int cur = nextNum(s, i);
+        char last_op = ' ';
+        while (i != s.length()) {
+            if (s[i] == '*') cur *= nextNum(s, ++i);
+            else if (s[i] == '/') cur /= nextNum(s, ++i);
+            else if (last_op == '+') result += cur, last_op=s[i], cur = nextNum(s, ++i);
+            else if (last_op == '-') result -= cur, last_op=s[i], cur = nextNum(s, ++i);
+            else result = cur, last_op = s[i], cur = nextNum(s, ++i);
         }
-        return sd.top();
-    }
-    int calc(int a, int b, char op) {
-        switch(op) {
-            case '+': return a+b;
-            case '-': return a-b;
-            case '*': return a*b;
-            case '/': return a/b;
-        }
-    }    
-    bool isoperator(char ch) {
-        return ch < '0' || ch > '9';
-    }
-    int diff(char p1, char p2) {
-        if (p1 == '(') {
-            if (p2 == ')') return -1;
-            return 1;
-        }
-        if (p2 == '(')
-            return 1;
-        if (p2 == ')')
-            return -1;
-        if (p1 == '+' || p1 == '-') {
-            if (p2 == '*' || p2 == '/') return 1;
-            return -1;
-        }
-        if (p1 == '*' || p1 == '/') return -1;
+        if (last_op == ' ') return cur;
+        else if (last_op == '+') return result+cur;
+        else return result-cur;
     }
 };
