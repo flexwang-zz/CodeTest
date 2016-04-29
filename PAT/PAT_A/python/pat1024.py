4d7b8ad0b177940346a45fea11731e35c65ceae3
flexwang <zhaotianzju@gmail.com>
2016-04-29 23:37:58 +0800

    pat

--- PAT/PAT_A/python/pat1024.py
+++ PAT/PAT_A/python/pat1024.py
@@ -1,12 +1,8 @@
 n, k = map(int, input().split())
 is_pal = lambda x: str(x) == str(x)[::-1]
 rev = lambda x: int(str(x)[::-1])
-if is_pal(n):
-    print('%d\n%d'%(n, 0))
-    exit()
-for i in range(1, k+1):
+i = 0
+while not is_pal(n) and i < k:
     n += rev(n)
-    if is_pal(n):
-        k = i
-        break
-print('%d\n%d'%(n, k));
+    i += 1
+print('%d\n%d'%(n, i));
