e1d3e593a979c192397f5819c80276258e47273f
flexwang <zhaotianzju@gmail.com>
2016-05-12 17:26:05 +0800

    pat

--- PAT/PAT_A/c++/pat1065.cpp
+++ PAT/PAT_A/c++/pat1065.cpp
@@ -1,103 +1,48 @@
-﻿
-#include<stdio.h>
+﻿/*
+Given three integers A, B and C in [-2^63, 2^63], you are supposed to tell whether A+B > C.
 
-#define Max(a,b,c) (a>b&&a>c?a:(b>c?b:c))
+Input Specification:
 
-int strlen(char *);
+The first line of the input gives the positive number of test cases, T (<=10). Then T test cases follow, each consists of a single line containing three integers A, B and C, separated by single spaces.
 
-int main() {
-	int repeat;
-	char index[] = "0123456789";
+Output Specification:
 
-	scanf("%d", &repeat);
+For each test case, output in one line "Case #X: true" if A+B>C, or "Case #X: false" otherwise, where X is the case number (starting from 1).
 
-	for (int i = 0; i < repeat; i++) {
-		char a[64], b[64], c[64], d[64];
+Sample Input:
+3
+1 2 3
+2 3 4
+9223372036854775807 -9223372036854775808 0
+Sample Output:
+Case #1: false
+Case #2: true
+Case #3: false
+*/
+#include <iostream>
+#include <stdio.h>
 
-		scanf("%s %s %s", a, b, c);
-		//  printf("%s %s %s\n", a, b, c);
+using namespace std;
 
-		unsigned int A1, B1, C1, A2, B2, C2;
-		int sa, sb, sc, sd;
+typedef long long ll;
 
-		sa = (a[0] == '-') ? -1 : 1;
-		sb = (b[0] == '-') ? -1 : 1;
-		sc = (c[0] == '-') ? -1 : 1;
+bool solve(ll a, ll b, ll c)
+{
+	ll sum = a+b;
+	if (a > 0 && b > 0 && sum <= 0) return true;
+	if (a < 0 && b < 0 && sum >= 0) return false;
+	return sum > c;
+}
 
-		int lena = strlen(a);
-		int lenb = strlen(b);
-		int lenc = strlen(c);
-
-		if (sa < 0) {
-			for (int j = 0; j < lena; j++) {
-				a[j] = a[j + 1];
-			}
-		}
-		if (sb < 0) {
-			for (int j = 0; j < lenb; j++) {
-				b[j] = b[j + 1];
-			}
-		}
-
-		if (sc < 0) {
-			for (int j = 0; j < lenc; j++) {
-				c[j] = c[j + 1];
-			}
-		}
-		int len = Max(lena, lenb, lenc);
-
-		bool flag = false;
-		for (int j = 0; j < len; j++) {
-			int ma = 0, mb = 0, mc = 0;
-			if (j + lena >= len) {
-				if (a[j + lena - len] != '-') {
-					//printf("%c", a[len-j-lena]);
-					ma = a[j + lena - len] - '0';
-					ma *= sa;
-				}
-
-			}
-			if (j + lenb >= len) {
-				if (b[-len + j + lenb] != '-') {
-					mb = b[-len + j + lenb] - '0';
-					mb *= sb;
-				}
-
-			}
-			if (j + lenc >= len) {
-				if (c[-len + j + lenc] != '-') {
-					mc = c[-len + j + lenc] - '0';
-					mc *= sc;
-				}
-			}
-
-			//printf("%d %d %d\n", ma, mb, mc);
-			if (ma + mb > mc) {
-				flag = true;
-				break;
-			} else if (ma + mb < mc) {
-				flag = false;
-				break;
-			}
-		}
-		if (!flag) {
-			printf("Case #%d: false\n", i + 1);
-		} else {
-			printf("Case #%d: true\n", i + 1);
-		}
-
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i=0; i<n; ++i) {
+		ll a, b, c;
+		scanf("%lld %lld %lld", &a, &b, &c);
+		if (solve(a, b, c)) printf("Case #%d: true\n", i+1);
+		else printf("Case #%d: false\n", i+1);
 	}
-
 	return 0;
 }
-
-int strlen(char *str) {
-	int len = 0;
-	while (str[len] != ' ' && str[len] != '\0')
-		len++;
-
-	if (str[0] == '-')
-		len--;
-	return len;
-}
-
--- PAT/PAT_A/c++/pat1069.cpp
+++ PAT/PAT_A/c++/pat1069.cpp
@@ -30,56 +30,28 @@
 
 
  */
-#include<stdio.h>
-#include<vector>
-#include<algorithm>
+#include <stdio.h>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int foo(int);
+int foo(int v) {
+	string s = to_string(v);
+	s.resize(4, '0');
+	sort(s.begin(), s.end());
+	string t = s;
+	reverse(t.begin(), t.end());
+	int a = stoi(s), b = stoi(t);
+	printf("%04d - %04d = %04d\n", b, a, b - a);
+	return b - a;
+}
 
 int main() {
 	int n;
-
 	scanf("%d", &n);
-
-	while ((n = foo(n))) {
-		if (n == 6174) {
-			break;
-		}
-	}
-
-//  system("pause");
-
+	while ((n = foo(n)) && n != 6174);
 	return 0;
-
 }
 
-int foo(int v) {
-	vector<int> digits;
-
-	while (v > 0) {
-		digits.push_back(v % 10);
-		v /= 10;
-	}
-
-	for (int i = digits.size(); i < 4; i++) {
-		digits.push_back(0);
-	}
-
-	int big = 0;
-	int small = 0;
-
-	sort(digits.begin(), digits.end());
-
-	for (int i = 0; i < 4; i++) {
-		small = small * 10 + digits[i];
-		big = big * 10 + digits[3 - i];
-	}
-
-	//7766 - 6677 = 1089
-	printf("%04d - %04d = %04d\n", big, small, big - small);
-
-	return big - small;
-}
 
